ed69a9a3022d240cde02b3df24e63ef28b6facba
huzcodes <huzcodes@outlook.com>
2024-03-28 03:16:15 +0400

    adding infrastructure registration class that holds the app db context registration with sql server

---
+++ CraftIQ.Inventory.Infrastructure/InfrastructureRegistrations.cs
@@ -0,0 +1,15 @@
+﻿using CraftIQ.Inventory.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CraftIQ.Inventory.Infrastructure
+{
+    public static class InfrastructureRegistrations
+    {
+        /// <summary>
+        /// this function for adding sql server db context connection
+        /// </summary>
+        public static void AddInventoryDbContext(this IServiceCollection services, string connectionString) =>
+            services.AddDbContext<AppDbContext>(o => o.UseSqlServer(connectionString));
+    }
+}
