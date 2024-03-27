a684437e964b4fd30a29a914d6f342d2dd36bc1d
huzcodes <huzcodes@outlook.com>
2024-03-28 03:14:43 +0400

    adding routes class for holding all the endpoints routes

---
+++ CraftIQ.Inventory.API/Routes.cs
@@ -0,0 +1,10 @@
+﻿namespace CraftIQ.Inventory.API
+{
+    public static class Routes
+    {
+        public class CategoriesRoutes
+        {
+            public const string CreateCategory = "/category";
+        }
+    }
+}
