599286c4f1bf1ffe6a1278e0c5343bde02850c49
huzcodes <huzcodes@outlook.com>
2024-03-28 03:15:27 +0400

    adding app db context for managing db sets and their configuration

---
+++ CraftIQ.Inventory.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using CraftIQ.Inventory.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace CraftIQ.Inventory.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        // db sets lands here
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<Core.Entities.Inventory> Inventories => Set<Core.Entities.Inventory>();
+        public DbSet<Order> Orders => Set<Order>();
+        public DbSet<OrderDetail> OrderDetails => Set<OrderDetail>();
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Transaction> Transactions => Set<Transaction>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
