2fe184b34c31208255467e61e155201cca7a4302
huzcodes <huzcodes@outlook.com>
2024-03-28 03:18:51 +0400

    calling the infrastrcuter registration in program file

--- CraftIQ.Inventory.API/Program.cs
+++ CraftIQ.Inventory.API/Program.cs
@@ -1,3 +1,5 @@
+using CraftIQ.Inventory.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// adding db context registration
+var inventoryDbConnectionString = builder.Configuration.GetSection("ConnectionStrings:InventoryDbConnection");
+builder.Services.AddInventoryDbContext(inventoryDbConnectionString.Value!);
 
 var app = builder.Build();
 
