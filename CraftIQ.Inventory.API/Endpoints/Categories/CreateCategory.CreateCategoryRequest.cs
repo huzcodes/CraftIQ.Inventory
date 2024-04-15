b961ae626921f0a3181d430ee1fc3d8c02a268ff
huzcodes <huzcodes@outlook.com>
2024-04-15 04:31:22 +0400

    removing test class

--- CraftIQ.Inventory.API/Endpoints/Categories/CreateCategory.CreateCategoryRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CraftIQ.Inventory.API.Endpoints.Categories
-{
-    public class CreateCategoryRequest
-    {
-        public const string Route = "/category";
-        public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-    }
-}
