37ae3fef4fccd2fda0133697824c5d4dd61babb4
huzcodes <huzcodes@outlook.com>
2024-04-28 16:21:12 +0400

    - Adding contracts folder into shared lib - Adding categories contracts

---
+++ CraftIQ.Inventory.Shared/Contracts/Categories/CategoriesContract.cs
@@ -0,0 +1,30 @@
+﻿namespace CraftIQ.Inventory.Shared.Contracts.Categories
+{
+    public class CategoriesContract
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public Guid CreatedBy { get; set; }
+        public Guid ModifiedBy { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+        public DateTimeOffset ModifiedOn { get; set; }
+
+        public CategoriesContract(Guid categoryId,
+                                  string name,
+                                  string description,
+                                  Guid createdBy,
+                                  Guid modifiedBy,
+                                  DateTimeOffset createdOn,
+                                  DateTimeOffset modifiedOn)
+        {
+            CategoryId = categoryId;
+            Name = name;
+            Description = description;
+            CreatedBy = createdBy;
+            CreatedOn = createdOn;
+            ModifiedBy = modifiedBy;
+            ModifiedOn = modifiedOn;
+        }
+    }
+}
---
+++ CraftIQ.Inventory.Shared/Contracts/Categories/CategoriesOperationsContract.cs
@@ -0,0 +1,14 @@
+﻿namespace CraftIQ.Inventory.Shared.Contracts.Categories
+{
+    public class CategoriesOperationsContract
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public CategoriesOperationsContract(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
