4b002650597cd05220215a4aa5881cd2d865eb44
huzcodes <huzcodes@outlook.com>
2024-06-08 14:07:07 +0400

    - Adding inventory service caller in inventory factory builder

--- CraftIQ.Inventory.Services/Factories/InventoryFactory.cs
+++ CraftIQ.Inventory.Services/Factories/InventoryFactory.cs
@@ -2,15 +2,17 @@
 using CraftIQ.Inventory.Core.Entities.Products;
 using CraftIQ.Inventory.Core.Interfaces;
 using CraftIQ.Inventory.Services.CategoriesImplementations;
+using CraftIQ.Inventory.Services.InventoryImplementations;
 using CraftIQ.Inventory.Services.ProductsImplementations;
 using huzcodes.Persistence.Interfaces.Repositories;
 
 namespace CraftIQ.Inventory.Services.Factories
 {
-    public class InventoryFactory<TRequest, TResponse>(IRepository<Category> categoryRepository, IRepository<Product> productRepository)
+    public class InventoryFactory<TRequest, TResponse>(IRepository<Category> categoryRepository, IRepository<Product> productRepository, IRepository<Core.Entities.Inventories.Inventory> inventoryRepository)
     {
         private readonly IRepository<Category> _categoryRepository = categoryRepository;
         private readonly IRepository<Product> _productRepository = productRepository;
+        private readonly IRepository<Core.Entities.Inventories.Inventory> _inventoryRepository = inventoryRepository;
         public IGenericServices<TRequest, TResponse> Build(string key)
         {
             switch (key)
@@ -19,6 +21,8 @@ namespace CraftIQ.Inventory.Services.Factories
                     return new CategoriesServices<TRequest, TResponse>(_categoryRepository);
                 case nameof(Product):
                     return new ProductsServices<TRequest, TResponse>(_categoryRepository, _productRepository);
+                case nameof(Core.Entities.Inventories.Inventory):
+                    return new InventoryServices<TRequest, TResponse>(_inventoryRepository);
                 default: return null!;
             }
         }
