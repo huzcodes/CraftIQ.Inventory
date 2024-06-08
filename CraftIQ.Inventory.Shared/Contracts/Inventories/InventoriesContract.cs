c7b904929917ea7093e8482845a256c938488d00
huzcodes <huzcodes@outlook.com>
2024-06-08 14:08:16 +0400

    - Adding inventory contract and operations contract

---
+++ CraftIQ.Inventory.Shared/Contracts/Inventories/InventoriesContract.cs
@@ -0,0 +1,27 @@
+﻿namespace CraftIQ.Inventory.Shared.Contracts.Inventories
+{
+    public class InventoriesContract : InventoriesOperationsContract
+    {
+        public Guid CreatedBy { get; set; }
+        public Guid ModifiedBy { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+        public DateTimeOffset ModifiedOn { get; set; }
+        public InventoriesContract(Guid inventoryId,
+                                   int quantity,
+                                   int reorderlevel,
+                                   string location,
+                                   Guid createdBy,
+                                   Guid modifiedBy,
+                                   DateTimeOffset createdOn,
+                                   DateTimeOffset modifiedOn) : base(inventoryId,
+                                                                     quantity,
+                                                                     reorderlevel,
+                                                                     location)
+        {
+            CreatedBy = createdBy;
+            ModifiedBy = modifiedBy;
+            CreatedOn = createdOn;
+            ModifiedOn = modifiedOn;
+        }
+    }
+}
---
+++ CraftIQ.Inventory.Shared/Contracts/Inventories/InventoriesOperationsContract.cs
@@ -0,0 +1,23 @@
+﻿namespace CraftIQ.Inventory.Shared.Contracts.Inventories
+{
+    public class InventoriesOperationsContract
+    {
+        public Guid _InventoryId { get; set; }
+        public int Quantity { get; set; }
+        public int Reorderlevel { get; set; }
+        public string Location { get; set; } = string.Empty;
+        public DateTimeOffset LastUpdated { get; set; }
+
+        public InventoriesOperationsContract(Guid inventoryId,
+                                             int quantity,
+                                             int reorderlevel,
+                                             string location)
+        {
+            _InventoryId = inventoryId;
+            Quantity = quantity;
+            Reorderlevel = reorderlevel;
+            Location = location;
+            LastUpdated = DateTimeOffset.Now;
+        }
+    }
+}
