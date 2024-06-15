e2b488f1edeccfb40c691de70855ffb69ffd0fa2
huzcodes <huzcodes@outlook.com>
2024-06-16 01:45:59 +0400

    - Adding factory builder navigation for transactions service implementations

--- CraftIQ.Inventory.Services/Factories/InventoryFactory.cs
+++ CraftIQ.Inventory.Services/Factories/InventoryFactory.cs
@@ -1,18 +1,24 @@
 ﻿using CraftIQ.Inventory.Core.Entities.Categories;
 using CraftIQ.Inventory.Core.Entities.Products;
+using CraftIQ.Inventory.Core.Entities.Transactions;
 using CraftIQ.Inventory.Core.Interfaces;
 using CraftIQ.Inventory.Services.CategoriesImplementations;
 using CraftIQ.Inventory.Services.InventoryImplementations;
 using CraftIQ.Inventory.Services.ProductsImplementations;
+using CraftIQ.Inventory.Services.TransactionsImplementations;
 using huzcodes.Persistence.Interfaces.Repositories;
 
 namespace CraftIQ.Inventory.Services.Factories
 {
-    public class InventoryFactory<TRequest, TResponse>(IRepository<Category> categoryRepository, IRepository<Product> productRepository, IRepository<Core.Entities.Inventories.Inventory> inventoryRepository)
+    public class InventoryFactory<TRequest, TResponse>(IRepository<Category> categoryRepository,
+                                                       IRepository<Product> productRepository,
+                                                       IRepository<Core.Entities.Inventories.Inventory> inventoryRepository,
+                                                       IRepository<Transaction> transactionRepository)
     {
         private readonly IRepository<Category> _categoryRepository = categoryRepository;
         private readonly IRepository<Product> _productRepository = productRepository;
         private readonly IRepository<Core.Entities.Inventories.Inventory> _inventoryRepository = inventoryRepository;
+        private readonly IRepository<Transaction> _transactionRepository = transactionRepository;
         public IGenericServices<TRequest, TResponse> Build(string key)
         {
             switch (key)
@@ -20,9 +26,11 @@ namespace CraftIQ.Inventory.Services.Factories
                 case nameof(Category):
                     return new CategoriesServices<TRequest, TResponse>(_categoryRepository);
                 case nameof(Product):
-                    return new ProductsServices<TRequest, TResponse>(_categoryRepository, _productRepository, _inventoryRepository);
+                    return new ProductsServices<TRequest, TResponse>(_categoryRepository, _productRepository, _inventoryRepository, _transactionRepository);
                 case nameof(Core.Entities.Inventories.Inventory):
                     return new InventoryServices<TRequest, TResponse>(_inventoryRepository);
+                case nameof(Transaction):
+                    return new TransactionsServices<TRequest, TResponse>(_transactionRepository);
                 default: return null!;
             }
         }
