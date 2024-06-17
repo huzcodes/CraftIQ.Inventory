83d5373cb12546c9d9b50060aed0ccc38964a547
huzcodes <huzcodes@outlook.com>
2024-06-17 21:46:53 +0400

    - Adding order details contract and operations contract in shared llb

---
+++ CraftIQ.Inventory.Shared/Contracts/OrderDetails/OrderDetailsContract.cs
@@ -0,0 +1,36 @@
+﻿namespace CraftIQ.Inventory.Shared.Contracts.OrderDetails
+{
+    public class OrderDetailsContract : OrderDetailsOperationsContract
+    {
+        public Guid CreatedBy { get; set; }
+        public Guid ModifiedBy { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+        public DateTimeOffset ModifiedOn { get; set; }
+
+        public OrderDetailsContract(Guid orderDetailId,
+                                    int quantity,
+                                    decimal totalPrice,
+                                    Guid orderId,
+                                    Guid productId)
+            : base(orderDetailId, quantity, totalPrice, orderId, productId)
+        { }
+
+        public OrderDetailsContract(Guid orderDetailId,
+                                    int quantity,
+                                    decimal totalPrice,
+                                    Guid orderId,
+                                    Guid productId,
+                                    Guid createdBy,
+                                    Guid modifiedBy,
+                                    DateTimeOffset createdOn,
+                                    DateTimeOffset modifiedOn)
+            : base(orderDetailId, quantity, totalPrice, orderId, productId)
+        {
+            CreatedBy = createdBy;
+            CreatedOn = createdOn;
+            ModifiedBy = modifiedBy;
+            ModifiedOn = modifiedOn;
+        }
+    }
+
+}
---
+++ CraftIQ.Inventory.Shared/Contracts/OrderDetails/OrderDetailsOperationsContract.cs
@@ -0,0 +1,25 @@
+﻿namespace CraftIQ.Inventory.Shared.Contracts.OrderDetails
+{
+    public class OrderDetailsOperationsContract
+    {
+        public Guid _OrderDetailId { get; set; } 
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
+        public Guid OrderId { get; set; }
+        public Guid ProductId { get; set; }
+
+        public OrderDetailsOperationsContract(Guid orderDetailId,
+                                              int quantity,
+                                              decimal totalPrice,
+                                              Guid orderId,
+                                              Guid productId)
+        {
+            _OrderDetailId = orderDetailId;
+            Quantity = quantity;
+            TotalPrice = totalPrice;
+            OrderId = orderId;
+            ProductId = productId;
+        }
+    }
+
+}
