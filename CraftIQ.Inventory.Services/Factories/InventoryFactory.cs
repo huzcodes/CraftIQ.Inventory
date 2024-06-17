8a3d36ea4e2366abf4770da707616a6f3cce55eb
huzcodes <huzcodes@outlook.com>
2024-06-17 21:50:57 +0400

    - Adding Factory Builder for Order service implementations. - Adding Factory Builder for Order Details service implementations.

--- CraftIQ.Inventory.Services/Factories/InventoryFactory.cs
+++ CraftIQ.Inventory.Services/Factories/InventoryFactory.cs
@@ -1,9 +1,13 @@
 ﻿using CraftIQ.Inventory.Core.Entities.Categories;
+using CraftIQ.Inventory.Core.Entities.OrderDetails;
+using CraftIQ.Inventory.Core.Entities.Orders;
 using CraftIQ.Inventory.Core.Entities.Products;
 using CraftIQ.Inventory.Core.Entities.Transactions;
 using CraftIQ.Inventory.Core.Interfaces;
 using CraftIQ.Inventory.Services.CategoriesImplementations;
 using CraftIQ.Inventory.Services.InventoryImplementations;
+using CraftIQ.Inventory.Services.OrderDetailsImplementations;
+using CraftIQ.Inventory.Services.OrdersImplementations;
 using CraftIQ.Inventory.Services.ProductsImplementations;
 using CraftIQ.Inventory.Services.TransactionsImplementations;
 using huzcodes.Persistence.Interfaces.Repositories;
@@ -13,12 +17,16 @@ namespace CraftIQ.Inventory.Services.Factories
     public class InventoryFactory<TRequest, TResponse>(IRepository<Category> categoryRepository,
                                                        IRepository<Product> productRepository,
                                                        IRepository<Core.Entities.Inventories.Inventory> inventoryRepository,
-                                                       IRepository<Transaction> transactionRepository)
+                                                       IRepository<Transaction> transactionRepository,
+                                                       IRepository<Order> orderRepository,
+                                                       IRepository<OrderDetail> orderDetailsRepository)
     {
         private readonly IRepository<Category> _categoryRepository = categoryRepository;
         private readonly IRepository<Product> _productRepository = productRepository;
         private readonly IRepository<Core.Entities.Inventories.Inventory> _inventoryRepository = inventoryRepository;
         private readonly IRepository<Transaction> _transactionRepository = transactionRepository;
+        private readonly IRepository<Order> _orderRepository = orderRepository;
+        private readonly IRepository<OrderDetail> _orderDetailsRepository = orderDetailsRepository;
         public IGenericServices<TRequest, TResponse> Build(string key)
         {
             switch (key)
@@ -31,6 +39,10 @@ namespace CraftIQ.Inventory.Services.Factories
                     return new InventoryServices<TRequest, TResponse>(_inventoryRepository);
                 case nameof(Transaction):
                     return new TransactionsServices<TRequest, TResponse>(_transactionRepository);
+                case nameof(Order):
+                    return new OrdersServices<TRequest, TResponse>(_orderRepository);
+                case nameof(OrderDetail):
+                    return new OrderDetailsServices<TRequest, TResponse>(_orderRepository, _productRepository, _orderDetailsRepository);
                 default: return null!;
             }
         }
