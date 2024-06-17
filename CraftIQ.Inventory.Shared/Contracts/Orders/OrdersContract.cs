7ae1894ee98a1a465b6435a7fee2ab30c8f4666a
huzcodes <huzcodes@outlook.com>
2024-06-17 21:44:26 +0400

    - Adding order contracts in shared llb

---
+++ CraftIQ.Inventory.Shared/Contracts/Orders/OrdersContract.cs
@@ -0,0 +1,42 @@
+﻿namespace CraftIQ.Inventory.Shared.Contracts.Orders
+{
+    public class OrdersContract : OrdersOperationsContract
+    {
+        public Guid CreatedBy { get; set; }
+        public Guid ModifiedBy { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+        public DateTimeOffset ModifiedOn { get; set; }
+
+        public OrdersContract(Guid orderId,
+                              Guid supplierId,
+                              DateTimeOffset orderDate,
+                              int totalAmount,
+                              int status,
+                              DateTimeOffset expectedDeliveryDate,
+                              int orderType,
+                              DateTimeOffset receivedDate)
+            : base(orderId, supplierId, orderDate, totalAmount, status, expectedDeliveryDate, orderType, receivedDate)
+        { }
+
+        public OrdersContract(Guid orderId,
+                              Guid supplierId,
+                              DateTimeOffset orderDate,
+                              int totalAmount,
+                              int status,
+                              DateTimeOffset expectedDeliveryDate,
+                              int orderType,
+                              DateTimeOffset receivedDate,
+                              Guid createdBy,
+                              Guid modifiedBy,
+                              DateTimeOffset createdOn,
+                              DateTimeOffset modifiedOn)
+            : base(orderId, supplierId, orderDate, totalAmount, status, expectedDeliveryDate, orderType, receivedDate)
+        {
+            CreatedBy = createdBy;
+            CreatedOn = createdOn;
+            ModifiedBy = modifiedBy;
+            ModifiedOn = modifiedOn;
+        }
+    }
+
+}
---
+++ CraftIQ.Inventory.Shared/Contracts/Orders/OrdersOperationsContract.cs
@@ -0,0 +1,34 @@
+﻿namespace CraftIQ.Inventory.Shared.Contracts.Orders
+{
+    public class OrdersOperationsContract
+    {
+        public Guid _OrderId { get; set; }
+        public Guid SupplierId { get; set; }
+        public DateTimeOffset OrderDate { get; set; }
+        public int TotalAmount { get; set; }
+        public int Status { get; set; }
+        public DateTimeOffset ExpectedDeliveryDate { get; set; }
+        public int OrderType { get; set; }
+        public DateTimeOffset ReceivedDate { get; set; }
+
+        public OrdersOperationsContract(Guid orderId,
+                                        Guid supplierId,
+                                        DateTimeOffset orderDate,
+                                        int totalAmount,
+                                        int status,
+                                        DateTimeOffset expectedDeliveryDate,
+                                        int orderType,
+                                        DateTimeOffset receivedDate)
+        {
+            _OrderId = orderId;
+            SupplierId = supplierId;
+            OrderDate = orderDate;
+            TotalAmount = totalAmount;
+            Status = status;
+            ExpectedDeliveryDate = expectedDeliveryDate;
+            OrderType = orderType;
+            ReceivedDate = receivedDate;
+        }
+    }
+
+}
